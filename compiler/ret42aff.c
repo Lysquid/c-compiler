2b533c706024308ca7dd361cc011f4010dc1f23d
Lysquid <romainbenoit21560@gmail.com>
2024-03-11 10:02:36 +0100

    clean repo

--- compiler/ret42aff.c
+++
@@ -1,6 +0,0 @@
-#include <inttypes.h>
-int main() {
-int a = 44;
-int b = a;
-return b;
-}
--- compiler/test.c
+++
@@ -1,8 +0,0 @@
-int main() {
-    int d,a;
-    a = -10;
-    int b = 10;
-    int c = -5;
-    d = -a+b+-c+-(+b+c+a)+10-23+a;
-    return d;
-}
