b461ca2f18570ff7d7312b15c8f4246414414a47
treelins <zeyang.kong@insa-lyon.com>
2024-03-11 09:54:01 +0100

    change repository

---
+++ tests/ifcc-test-output/testfiles-7_redefinition/input.c
@@ -0,0 +1,7 @@
+int main() {
+    int a;
+    a = 5;
+    a = 10;
+    int a = 10;
+    return a;
+}
--- tests/testfiles/8_expr_with_sign.c
+++ tests/testfiles/8_expr_with_sign.c
@@ -4,6 +4,6 @@ int main() {
     int b = 10;
     int c = -5;
     d = -a+b+-c-(+b+c+a)+-10-+23+a;
-    e = a/-c*+b+(a*b/c*a)+(a+-b+c-d)*-a;
+    e = a/-c*+b+-(a*b/c*a)-+(a+-b+c-d)*-a;
     return e;
 }
