04c28a5e4eb7ed79f0c1f53f608b299a72581171
mthsngn <mathis.nguyen@gmail.com>
2024-03-13 09:23:38 +0100

    correction test

--- tests/3_expressions/16_bit_to_bit_op.c
+++ tests/3_expressions/16_bit_to_bit_op.c
@@ -3,8 +3,8 @@ int main() {
     int b = 7;
     int c = 9;
 
-    int result1 = a * b + c;
-    int result2 = a & b | c;           
+    int result1 = a ^ b & c;
+    int result2 = a & b | c;             
 
     return result1 + result2;
 }
